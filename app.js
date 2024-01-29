1af4a661e3429cb1a3ebee2879fd799a6ffd51f1
NoorTushar <tushar.noor.khan@gmail.com>
2024-01-29 12:15:48 +0600

    importing the whole directory

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+/*
 const importedValues = require("./math");
 const orange = require("./fruits/orange");
 console.log(importedValues);
 console.log(orange);
+*/
+const info = require("./fruits");
+
+console.log(info);
