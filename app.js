f2a58678d6e97ca08d7171193f6c290ff58e9d38
NoorTushar <tushar.noor.khan@gmail.com>
2024-01-29 11:57:49 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const importedValues = require("./math");
+const orange = require("./fruits/orange");
+console.log(importedValues);
+console.log(orange);
---
+++ math.js
@@ -0,0 +1,13 @@
+exports.add = (a, b) => a + b;
+exports.subtract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.PI = 3.14;
+exports.taxRate = 0.12;
+
+// module.exports = {
+//    add,
+//    subtract,
+//    multiply,
+//    PI,
+//    taxRate,
+// };
